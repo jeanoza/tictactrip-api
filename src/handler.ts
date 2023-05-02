92ac24335ae4c08a1bbd83c09070efe209fc967e
jeanoza <choikb916@gmail.com>
2023-05-02 10:15:38 +0200

    fix:clean code

--- src/handler.ts
+++ src/handler.ts
@@ -1,53 +1,42 @@
 import { IncomingMessage, ServerResponse } from "http";
 
+/**
+ * J'ai essayé sans regex afin de mettre le dernier mot à la même position
+ * en ajoutant espace entre les mots.
+ * Ca n'a pas marché comme output.txt
+ * D'ailleurs, même quand j'ai utilisé une bibliothèque externe(word-wrap) pour voir,
+ * Elle ne donne pas non plus le même resultat que output.txt.
+ * Comme j'ai fait de mon mieux(pendant 2 jours), j'arrête ici.
+ * mais je veux bien que tu m'explique comment tu as fait:)
+ */
 function breakText(text: string) {
-	const MAX_LENGTH = 80;
-	let lines = [];
-	let currentLine = "";
-	let words = text.split(" ");
-
-	words.forEach((word) => {
-		if (currentLine.length + word.length <= MAX_LENGTH) {
-			// Add the word to the current line
-			currentLine += " " + word;
-		} else {
-			// Add the current line to the lines array and start a new line
-			lines.push(currentLine.trim());
-			currentLine = word;
-		}
-	});
-
-	// Add the last line to the lines array
-	lines.push(currentLine.trim());
-
-	// Join the lines array with newline characters and print the result
-	return lines.join("\n");
+  return text.replace(/(.{1,80})(\s+|$)/g, "$1\n");
 }
 
 export function root(req: IncomingMessage, res: ServerResponse) {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("root");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("root");
 }
 
 export function justify(req: IncomingMessage, res: ServerResponse) {
-	let body = "";
-	req.on("data", (chunk) => {
-		body += chunk.toString();
-	});
-	req.on("end", () => {
-		try {
-			let result = breakText(body);
-			res.writeHead(200, { "Content-Type": "text/plain" });
-			res.end(body);
-		} catch (error) {
-			console.error(error);
-			res.writeHead(400, { "Content-Type": "text/plain" });
-			res.end("Bad Request");
-		}
-	});
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    try {
+      let result = breakText(body);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(result);
+    } catch (error) {
+      console.error(error);
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end("Bad Request");
+    }
+  });
 }
 
 export function token(req: IncomingMessage, res: ServerResponse) {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("token");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("token");
 }
